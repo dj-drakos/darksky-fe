087228cf38a70df13075a8527fa7707f344763a3
DJ Drakos <dianajodrakos@gmail.com>
2021-07-06 10:45:14 -0700

    built router skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Header from './Header.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+
+    return (
+
+      <Router>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={LandingPage}/>
+            <Route exact path='/signup' component={SignUp}/>
+            <Route exact path='/main' component={Main}/>
+            <Route exact path='/astrolist' component={AstroList}/>
+            <Route exact path='/astrodetail' component={AstroDetail}/>
+            <Route exact path='/loglist' component={LogList}/>
+            <Route exact path='/logdetail' component={LogDetail}/>
+          </Switch>
+      </Router>
+    )
+  }
 }
-
-export default App;
