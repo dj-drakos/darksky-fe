6d649cc641be7908ff737b3afbbf71b62aec6cd3
Isaac <isaac.ewing3@gmail.com>
2021-07-07 16:17:32 -0600

    hiding api stuff

--- src/Main.js
+++ src/Main.js
@@ -5,6 +5,7 @@ export default class Main extends Component {
     render() {
         return (
             <div>
+                <img src='https://www.7timer.info/bin/astro.php?lon=-122.17&lat=45.09&ac=0&lang=en&unit=british&output=internal&tzshift=0' alt='yup'></img>
                 <h1>Main Page</h1>
                 <LocationPrompt></LocationPrompt>
             </div>
--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -32,7 +32,7 @@ export async function getSolarSystemAPI () {
 
 export async function getLocationAPI (city) {
     const { body } = await request 
-        .get(`https://us1.locationiq.com/v1/search.php?key=pk.00832111835699fdf09b6bafa0965ea8&q=${city}&format=json`)
+        .get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION}&q=${city}&format=json`)
     console.log(body);
     const location = { 
         latitude: body[0].lat, 
