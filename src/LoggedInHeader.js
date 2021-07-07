a02dc930298c8c2dc9812288a1a0992a4aa225c8
DJ Drakos <dianajodrakos@gmail.com>
2021-07-07 11:50:21 -0700

    header renders dynamically based on if user is logged in

--- src/LoggedInHeader.js
+++ src/LoggedInHeader.js
@@ -3,12 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 
 export default class Header extends Component {
+
     render() {
+
         return (
             <div className="header">
                 <NavLink to="/main">Astromapi</NavLink>
-                <NavLink to="/">Login</NavLink>
-                <NavLink to="/signup">Sign Up</NavLink>
+                <NavLink to="/astro-list">Search</NavLink>
+                <NavLink to="/journal-list">Journal</NavLink>
+                <NavLink to="/" onClick={this.props.logout}>Log Out</NavLink>
             </div>
         )
     }
