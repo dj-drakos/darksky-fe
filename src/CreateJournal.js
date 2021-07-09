ddbad3153338022db677628ce118daefe18da2ad
DJ Drakos <dianajodrakos@gmail.com>
2021-07-08 19:50:46 -0700

    create journal page styling done

--- src/CreateJournal.js
+++ src/CreateJournal.js
@@ -48,18 +48,22 @@ export default class CreateJournal extends Component {
     render() {
         return (
             <div className="main">
-                <form onSubmit={this.handleSubmit}>
+                <h1>Create Journal</h1>
+
+                <form className='journal-entry' onSubmit={this.handleSubmit}>
                     <label>
-                        <input placeholder="title" onChange={this.handleTitleInputChange} />
+                        <input className='title' placeholder="title" onChange={this.handleTitleInputChange} />
                     </label>
                     <textarea placeholder="observe anything interesting?" onChange={this.handleTextChange} >
                     </textarea>
-                    <label>
-                        <input onChange={this.handleImageInputChange} type="url" />
-                    </label>
-                    <button>
-                        Done
-                    </button>
+                    <div className='journal-image'>
+                        <label>
+                            <input onChange={this.handleImageInputChange} type="url" placeholder="image url"/>
+                        </label>
+                        <button>
+                            Done
+                        </button>
+                    </div>
                 </form>
             </div>
         )
