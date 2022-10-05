4314d79c17d5220630dab7e9da032e2cbe322c11
DJ Drakos <dianajodrakos@gmail.com>
2022-10-04 19:48:55 -0700

    refactor getSolarSystemAPI function to return munged data

--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -6,30 +6,43 @@ const locationURL = process.env.REACT_APP_LOCATION_URL
 const locationKey = process.env.REACT_APP_LOCATION_KEY
 
 export async function getSolarSystemAPI (pageNumber, filter, search) {
+    let searchResult = [];
     if(filter === 'all') {
         const solarSystemURL = `${systemeSolaireURL}/bodies?order=englishName,asc&page=${pageNumber},20&filter[]=englishName,sw,${search}`;
         const { body } = await request 
             .get(solarSystemURL)
-        return body.bodies;
+        searchResult = [...body.bodies]
     } 
     if(filter === 'planets'){
         const planetsURL = `${systemeSolaireURL}/bodies?order=englishName,asc&page=${pageNumber},20&filter[]=isPlanet,neq,&filter[]=englishName,sw,${search}`;
         const { body } = await request 
             .get(planetsURL)
-        return body.bodies;
+        searchResult = [...body.bodies]
     }
     if(filter === 'moons') {
         const moonsURL = `${systemeSolaireURL}/bodies?order=englishName,asc&page=${pageNumber},20&filter[]=aroundPlanet,neq,&filter[]=englishName,sw,${search}`;
         const { body } = await request 
             .get(moonsURL)
-        return body.bodies;
+        searchResult = [...body.bodies]
     }
     if(filter === 'other') {
         const othersURL = `${systemeSolaireURL}/bodies?order=englishName,asc&page=${pageNumber},20&filter[]=isPlanet,eq,&filter[]=aroundPlanet,eq,&filter[]=id,sw,${search}`;
         const { body } = await request 
             .get(othersURL)
-        return body.bodies;
+        searchResult = [...body.bodies]
     }
+    return searchResult.map(item => { return {
+        id: item.id,
+        name: item.englishName ?? item.id,
+        bodyType: item.bodyType,
+        discoveryDate: item.discoveryDate,
+        gravity: item.gravity,
+        radius: item.meanRadius,
+        moons: item.moons,
+        aroundPlanet: item.aroundPlanet
+        }
+    })
+
 }
 
 export async function getLocationAPI (city) {
