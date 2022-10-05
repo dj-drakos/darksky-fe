25c4398a8817c6a36475063d19b7f7e35774a760
DJ Drakos <dianajodrakos@gmail.com>
2022-10-05 11:55:45 -0700

    refactor dashboard children to be functional components

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getApodAPI } from '../utils/api-utils.js'
 
-
-
-export default class Apod extends Component {
-    state = {
+export default function Apod() {
+    const [{description, media_type, title, url}, setState] = useState({
         description: '',
         media_type: '',
         title: '',
         url: ''
-    }
-    apod = async () => {
-        const apodRes = await getApodAPI();
-        this.setState({ 
-            description: apodRes.explanation,
-            media_type: apodRes.media_type, 
-            title: apodRes.title,
-            url: apodRes.url })
-    }
+    })
 
-    componentDidMount = async () => {
-        await this.apod()
-    }
-    render() {
-        return (
-            <div className="flex-element">
-                <h2>NASA Astronomy Picture of the Day</h2>
-                {this.state.media_type === 'video' ? <iframe src={this.state.url} title='video of the day'></iframe> : 
-                <img src={this.state.url} alt='img of the day'></img>}
-                <h4>{this.state.title}</h4>
-                <p className='apod'>{this.state.description}</p>
-            </div>
-        )
-    }
+    useEffect(() => {
+        getApodAPI()
+            .then((res) => setState({ 
+                description: res.explanation,
+                media_type: res.media_type, 
+                title: res.title,
+                url: res.url }))
+    }, [])
+
+    return (
+        <div className="flex-element">
+            <h2>NASA Astronomy Picture of the Day</h2>
+            {media_type === 'video' ? <iframe src={url} title='video of the day'></iframe> : 
+            <img src={url} alt='img of the day'></img>}
+            <h4>{title}</h4>
+            <p className='apod'>{description}</p>
+        </div>
+    )
 }
--- src/components/LocationPrompt.js
+++ src/components/LocationPrompt.js
@@ -1,57 +1,79 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { getLocationAPI } from '../utils/api-utils'
 import { setLocation, getLocation } from '../utils/local-storage-utils'
 
 const sevenTimerURL = process.env.REACT_APP_7TIMER_URL
-export default class LocationPrompt extends React.Component {
-    state = {
-        location: '',
-        url: ''
-    }
 
-    locationChange = e => {
-        this.setState({ 
-            location: e.target.value
+export default function LocationPrompt() {
+    const [{name, latitude, longitude}, setState] = useState({
+        name: '',
+        latitude: 0,
+        longitude: 0,
         })
+
+    useEffect(() => {
+        const localStorageLocation = getLocation()
+        setState(localStorageLocation)
+    }, [])
+
+    const locationChange = e => {
+        setState((state) => ({
+            ...state,
+            name: e.target.value
+        }))
     }
     
-    urlChange = e => {
-        this.setState({  
-            url: `${sevenTimerURL}?lon=${this.state.location.longitude}&lat=${this.state.location.latitude}&ac=0&lang=en&unit=british&output=internal&tzshift=0` 
-        })
-    }
-    
-    handleSubmit = async e => {
+    const handleSubmit = async e => {
         e.preventDefault();
-        setLocation(await getLocationAPI(this.state.location))
-        this.urlChange()
+        const newLocation = await getLocationAPI(name)
+        setLocation(newLocation)
+        setState(newLocation)
     }
 
-    render() {
-        const location = getLocation();
-        return (
-            <div className="flex-element">
-                <h2>Viewing Conditions Forecast</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label>
-                            <input type="text" placeholder='location' onChange={this.locationChange}></input>
-                        </label>
-                        <button>Set</button><br/>
-                    </div>
-                    <img src={`${sevenTimerURL}?lon=${location.longitude}&lat=${location.latitude}&ac=0&lang=en&unit=british&output=internal&tzshift=0`} alt='viewing conditions' />
-                </form>
-                <div className='weather-key'>
-                    <h4>Key:</h4>
-                    <p><span>clear</span> <img src='./about_astro_cloud.png' alt='cloud cover' /> cloudy</p>
-                    <p><span>clear</span><img src='./about_astro_seeing.png' alt='astronomical seeing' />turbulent</p>
-                    <p><span>clear</span><img src='./about_astro_transparency.png' alt='atmospheric transparency' />hazy</p>
-                    <p><span>stable</span><img src='./about_astro_unstable.png' alt='astronomical instability' />unstable</p>
-                    <p><span>dry</span> <img src='./about_astro_rh.png' alt='relative humidity' />humid</p>
-                    <p><span>calm</span><img src='./about_astro_wind.png' alt='wind' />strong</p>
-                    <p><span>rain / snow</span><img src='./about_astro_rainsnow.png' alt='chance rain or snow' /></p>
+    return (
+        <div className="flex-element">
+            <h2>Viewing Conditions Forecast</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        <input type="text" placeholder='location' value={name} onChange={locationChange} />
+                    </label>
+                    <button>Set</button>
+                    <br/>
                 </div>
+                <img src={`${sevenTimerURL}?lon=${longitude}&lat=${latitude}&ac=0&lang=en&unit=british&output=internal&tzshift=0`} alt='viewing conditions' />
+            </form>
+            <div className='weather-key'>
+                <h4>Key:</h4>
+                <p>
+                    <span>clear</span> 
+                    <img src='./about_astro_cloud.png' alt='cloud cover' /> cloudy
+                </p>
+                <p>
+                    <span>clear</span>
+                    <img src='./about_astro_seeing.png' alt='astronomical seeing' />turbulent
+                </p>
+                <p>
+                    <span>clear</span>
+                    <img src='./about_astro_transparency.png' alt='atmospheric transparency' />hazy
+                    </p>
+                <p>
+                    <span>stable</span>
+                    <img src='./about_astro_unstable.png' alt='astronomical instability' />unstable
+                </p>
+                <p>
+                    <span>dry</span> 
+                    <img src='./about_astro_rh.png' alt='relative humidity' />humid
+                </p>
+                <p>
+                    <span>calm</span>
+                    <img src='./about_astro_wind.png' alt='wind' />strong
+                </p>
+                <p>
+                    <span>rain / snow</span>
+                    <img src='./about_astro_rainsnow.png' alt='chance rain or snow' />
+                </p>
             </div>
-        )
-    }
+        </div>
+    )
 } 
